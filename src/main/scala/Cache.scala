f12fb3e2b3e270f2da6b9f9704ab3c9f0f7b3a91
Scala Steward <me@scala-steward.org>
2022-05-13 21:14:22 +0200

    Reformat with scalafmt 3.5.3

--- src/main/scala/Cache.scala
+++ src/main/scala/Cache.scala
@@ -71,7 +71,7 @@ object Exploration2 {
   )
 }
 
-final case class Lookup[-Key, -R, +E, +Value](value: Key => ZIO[R, E, Value]) extends (Key => ZIO[R, E, Value]) {
+final case class Lookup[-Key, -R, +E, +Value](value: Key => ZIO[R, E, Value]) extends Key => ZIO[R, E, Value] {
   def apply(v1: Key): ZIO[R, E, Value] = value(v1)
 }
 trait Cache[-Key, +E, +Value] {
